3faf5c707afc6bb73e349f77dfdfb1161847eaf3
Pitrified <nobilipietro@gmail.com>
2024-08-10 00:55:49 +0200

    Add experiments with direct ollama call

---
+++ scratch_space/llm_/ollama_base_async.py
@@ -0,0 +1,29 @@
+"""Test the vanilla Ollama chatbot with async calls"""
+
+import asyncio
+
+from loguru import logger as lg
+from ollama import AsyncClient
+
+
+async def main_loop() -> None:
+    lg.info("Starting game loop")
+    ac = AsyncClient()
+
+    model_name = "phi3"
+    ac_res = await ac.chat(
+        model=model_name,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that translates english to spanish.",
+            },
+            {"role": "human", "content": "I love programming."},
+        ],
+    )
+
+    lg.info(f"Response: {ac_res}")
+
+
+# Run the main loop
+asyncio.run(main_loop())
