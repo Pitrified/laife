0392c15f33de6a9a0468449522739d1b299cba74
Pitrified <nobilipietro@gmail.com>
2024-08-17 12:48:33 +0200

    Swap World and WorldRenderer dependency

--- scratch_space/game/world_loop.py
+++ scratch_space/game/world_loop.py
@@ -117,38 +117,52 @@ class World:
 
     def __init__(self) -> None:
         """Initialize the world."""
+        self.wr = WorldRenderer()
         self.players = []
-        self.player_tasks = []
         self.add_player()
         self.add_prob = 0.01
 
-    async def simulate(self) -> None:
-        """Simulate the world."""
+    async def main_loop(self) -> None:
+        """Run the main loop."""
+        alg.log("W: Starting game loop")
         while True:
-            # alg.log("SIMULATE: Simulating the world")
-            for player in self.players:
-                if player.state == "idle":
-                    if not player.input_queue.empty():
-                        alg.log(f"SIMULATE: {player.name} is idle but has input")
-                        continue
-                    # this would be the place where the WORLD decides what to do
-                    if random.randint(0, 1):
-                        alg.log(f"SIMULATE: Adding think to {player.name}")
-                        await player.input_queue.put("think")
-                    else:
-                        alg.log(f"SIMULATE: Adding execute to {player.name}")
-                        await player.input_queue.put("execute")
-            if len(self.players) < 1 and random.random() < self.add_prob:
-                alg.log(f"SIMULATE: Adding a player {self.add_prob}")
-                self.add_player()
-                self.add_prob /= 2
-            await asyncio.sleep(0)
+            alg.log("W: Running game loop")
+            await self.simulate()
+            self.render()
+            await asyncio.sleep(0.1)
+
+    async def simulate(self) -> None:
+        """Simulate the agents in the world."""
+        # alg.log("SIMULATE: Simulating the world")
+        for player in self.players:
+            # find an idle player
+            if player.state != "idle":
+                continue
+            # an idle player should have no input in the queue
+            if not player.input_queue.empty():
+                alg.log(f"SIMULATE: {player.name} is idle but has input")
+                continue
+            # this would be the place where the WORLD decides what to do
+            if random.randint(0, 1):
+                alg.log(f"SIMULATE: Adding think to {player.name}")
+                await player.input_queue.put("think")
+            else:
+                alg.log(f"SIMULATE: Adding execute to {player.name}")
+                await player.input_queue.put("execute")
+        if len(self.players) < 1 and random.random() < self.add_prob:
+            alg.log(f"SIMULATE: Adding a player {self.add_prob}")
+            self.add_player()
+            self.add_prob /= 2
+
+    def render(self) -> None:
+        """Render the world."""
+        self.wr.render()
 
     def add_player(self) -> None:
         """Add a player to the world."""
         player = Player(f"p{len(self.players)}")
         self.players.append(player)
-        self.player_tasks.append(asyncio.create_task(player.play()))
+        asyncio.create_task(player.play())
 
 
 class WorldRenderer:
@@ -165,10 +179,6 @@ class WorldRenderer:
         self.redraw_period_sec = 1
         # set the redraw deadline to now so that the world is drawn immediately
         self.redraw_deadline = time.time()
-
-        # initialize the world
-        self.world = World()
-        asyncio.create_task(self.world.simulate())
 
     def reset_deadline(self) -> None:
         """Reset the redraw deadline."""
@@ -194,22 +204,16 @@ class WorldRenderer:
         pygame.display.flip()
         self.reset_deadline()
 
-    async def main_loop(self) -> NoReturn:
-        """Run the main loop."""
-        alg.log("WR: Starting game loop")
-
-        while True:
-            alg.log("MAIN: Running game loop")
-            self.check_events()
-            self.redraw()
-            # yield control to the event loop
-            await asyncio.sleep(0.1)
+    def render(self) -> None:
+        """Render the world."""
+        self.check_events()
+        self.redraw()
 
 
 async def main() -> NoReturn:
     """Main function."""
-    world_renderer = WorldRenderer()
-    await world_renderer.main_loop()
+    world = World()
+    await world.main_loop()
 
 
 if __name__ == "__main__":
