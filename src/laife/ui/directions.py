fdd0f99a98ead08ed67f6a4dad0327087ca0ee13
Pitrified <nobilipietro@gmail.com>
2024-08-19 16:15:23 +0200

    Refactor cardinal direction conversion functions to handle inverted y-axis

--- src/laife/ui/directions.py
+++ src/laife/ui/directions.py
@@ -6,7 +6,7 @@ import math
 from laife.config.types import Position
 
 
-class Cardinal(Enum):
+class CardinalDirection(Enum):
     North = "north"
     South = "south"
     East = "east"
@@ -17,37 +17,40 @@ class Cardinal(Enum):
     SouthWest = "southwest"
 
 
-def pospos2cardinal(you: Position, target: Position) -> Cardinal:
+def pospos2cardinal_direction(you: Position, target: Position) -> CardinalDirection:
     """Convert two positions to cardinal coordinates.
 
     The cardinal coordinates are based on the direction from you to the target.
     """
-    card_float = pospos2cardinal_float(you, target)
-    if 0 * 45 + 22.5 <= card_float < 1 * 45 + 22.5:
-        return Cardinal.NorthEast
-    elif 1 * 45 + 22.5 <= card_float < 2 * 45 + 22.5:
-        return Cardinal.East
-    elif 2 * 45 + 22.5 <= card_float < 3 * 45 + 22.5:
-        return Cardinal.SouthEast
-    elif 3 * 45 + 22.5 <= card_float < 4 * 45 + 22.5:
-        return Cardinal.South
-    elif 4 * 45 + 22.5 <= card_float < 5 * 45 + 22.5:
-        return Cardinal.SouthWest
-    elif 5 * 45 + 22.5 <= card_float < 6 * 45 + 22.5:
-        return Cardinal.West
-    elif 6 * 45 + 22.5 <= card_float < 7 * 45 + 22.5:
-        return Cardinal.NorthWest
+    cardinal_degrees = pospos2cardinal_degrees(you, target)
+    if 0 * 45 + 22.5 <= cardinal_degrees < 1 * 45 + 22.5:
+        return CardinalDirection.NorthEast
+    elif 1 * 45 + 22.5 <= cardinal_degrees < 2 * 45 + 22.5:
+        return CardinalDirection.East
+    elif 2 * 45 + 22.5 <= cardinal_degrees < 3 * 45 + 22.5:
+        return CardinalDirection.SouthEast
+    elif 3 * 45 + 22.5 <= cardinal_degrees < 4 * 45 + 22.5:
+        return CardinalDirection.South
+    elif 4 * 45 + 22.5 <= cardinal_degrees < 5 * 45 + 22.5:
+        return CardinalDirection.SouthWest
+    elif 5 * 45 + 22.5 <= cardinal_degrees < 6 * 45 + 22.5:
+        return CardinalDirection.West
+    elif 6 * 45 + 22.5 <= cardinal_degrees < 7 * 45 + 22.5:
+        return CardinalDirection.NorthWest
     else:
-        return Cardinal.North
+        return CardinalDirection.North
 
 
-def pospos2cardinal_float(you: Position, target: Position) -> float:
-    """Convert two positions to cardinal coordinates."""
+def pospos2cardinal_degrees(you: Position, target: Position) -> float:
+    """Convert two positions to cardinal coordinates.
+
+    Note that in pygame, the y-axis is flipped.
+    """
     dx = target[0] - you[0]
-    dy = target[1] - you[1]
+    dy = -(target[1] - you[1])
     if dx == 0 and dy == 0:
         return 0
-    radians = math.atan2(dy, dx)
-    deg = math.degrees(radians)
-    card_float = (90 - deg + 360) % 360
-    return card_float
+    polar_radians = math.atan2(dy, dx)
+    polar_degrees = math.degrees(polar_radians)
+    cardinal_degrees = (90 - polar_degrees) % 360
+    return cardinal_degrees
--- tests/ui/test_directions.py
+++ tests/ui/test_directions.py
@@ -2,18 +2,18 @@
 
 import pytest
 
-from laife.ui.directions import Cardinal, pospos2cardinal
+from laife.ui.directions import CardinalDirection, pospos2cardinal_direction
 
 
-def test_pospos2cardinal():
-    """Test that pospos2cardinal works."""
-    p1 = (0, 0)
-    assert pospos2cardinal(p1, (0, 1)) == Cardinal.North
-    assert pospos2cardinal(p1, (1, 1)) == Cardinal.NorthEast
-    assert pospos2cardinal(p1, (1, 0)) == Cardinal.East
-    assert pospos2cardinal(p1, (1, -1)) == Cardinal.SouthEast
-    assert pospos2cardinal(p1, (0, -1)) == Cardinal.South
-    assert pospos2cardinal(p1, (-1, -1)) == Cardinal.SouthWest
-    assert pospos2cardinal(p1, (-1, 0)) == Cardinal.West
-    assert pospos2cardinal(p1, (-1, 1)) == Cardinal.NorthWest
-    assert pospos2cardinal(p1, (0, 0)) == Cardinal.North
+def test_pospos2cardinal_direction():
+    """Test that pospos2cardinal_direction works."""
+    you = (0, 0)
+    assert pospos2cardinal_direction(you, (0, -1)) == CardinalDirection.North
+    assert pospos2cardinal_direction(you, (1, -1)) == CardinalDirection.NorthEast
+    assert pospos2cardinal_direction(you, (1, 0)) == CardinalDirection.East
+    assert pospos2cardinal_direction(you, (1, 1)) == CardinalDirection.SouthEast
+    assert pospos2cardinal_direction(you, (0, 1)) == CardinalDirection.South
+    assert pospos2cardinal_direction(you, (-1, 1)) == CardinalDirection.SouthWest
+    assert pospos2cardinal_direction(you, (-1, 0)) == CardinalDirection.West
+    assert pospos2cardinal_direction(you, (-1, -1)) == CardinalDirection.NorthWest
+    assert pospos2cardinal_direction(you, (0, 0)) == CardinalDirection.North
