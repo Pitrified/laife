8cc892fe4c9060d3a70eb92195acd20fdd2f77ac
Pitrified <nobilipietro@gmail.com>
2024-10-18 22:28:22 +0200

    Add Action entities for solving Missions

---
+++ src/laife/entities/action.py
@@ -0,0 +1,41 @@
+"""Actions are something the Brain decides to do to solve a Mission."""
+
+from pydantic import BaseModel, Field
+
+from laife.ui.directions import CardinalDirection
+
+
+class Action(BaseModel):
+    """Action to solve a Mission."""
+
+
+class ActionMove(Action):
+    """Move towards a direction."""
+
+    direction: CardinalDirection = Field(..., description="The direction to move.")
+    distance: int = Field(..., description="The distance to move.")
+
+
+class ActionBuild(Action):
+    """Build something."""
+
+    building_type: str = Field(..., description="The building to build.")
+    description: str = Field(..., description="The description of the building.")
+    size: int = Field(..., description="The size of the building.")
+
+
+class ActionCraft(Action):
+    """Craft something."""
+
+    item: str = Field(..., description="The item to craft.")
+    description: str = Field(..., description="The description of the item.")
+
+
+Actions = ActionMove | ActionBuild | ActionCraft
+
+
+class ActionOption(BaseModel):
+    """An action to take."""
+
+    action: Actions = Field(..., description="The action to take.")
+    reason: str = Field(..., description="The reason for the action.")
